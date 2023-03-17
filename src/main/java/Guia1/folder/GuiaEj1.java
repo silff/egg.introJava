4b8829f0412ebb6bbd22a1e0d18cfc78b561cc05
silff <128050929+silff@users.noreply.github.com>
2023-03-17 13:18:26 -0300

    Create GuiaEj1.java

---
+++ src/main/java/Guia1/folder/GuiaEj1.java
@@ -0,0 +1,29 @@
+/*Crear un proyecto de Java y definir al menos 6 variables en tu IDE de distintos tipos de datos.
+ */
+package Guia1.folder;
+
+/**
+ *
+ * @author sfappiano
+ */
+public class GuiaEj1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "sil";
+        char caracter = 'a';
+        boolean bandera = false;
+        int num1 = 21474836;
+        int num2 = 25;
+        long enteroLargo = 922337209;
+        float decimal = 1045111110;
+        double decDoble = 212.1231211321545441100000000000000000000000000000000004456454315634;
+        System.out.println(nombre + " " + caracter + " " + bandera + " " + num1 + " " + enteroLargo + " " + decimal + " " + decDoble);
+        
+        int suma = num1 + num2;
+        System.out.println(suma);
+    }
+    
+}
