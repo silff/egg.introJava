f1e009768640276c53aaf5028b1472141e7479ab
silff <128050929+silff@users.noreply.github.com>
2023-03-17 10:38:47 -0300

    Update Ejercicio1.java

--- src/main/java/Guia2/Ejercicio1.java
+++ src/main/java/Guia2/Ejercicio1.java
@@ -21,8 +21,12 @@ public class Ejercicio1 {
        int num1 = leer.nextInt();
        int num2 = leer.nextInt();
        System.out.println("los numeros son " + num1 + " y " + num2);
-       System.out.println("el mayor es ");
-       
+       if (num1 > num2){   
+       System.out.println("el mayor es " + num1);
+       } else if (num2 > num1) {
+            System.out.println("el mayor es " + num2);
+        }
     }
     
 }
+
