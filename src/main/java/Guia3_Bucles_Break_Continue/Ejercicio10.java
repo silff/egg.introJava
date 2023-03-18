8bd0e16ae6062ffca290014b77cb5c61f185bb88
silff <128050929+silff@users.noreply.github.com>
2023-03-18 13:25:34 -0300

    Update Ejercicio10.java

--- src/main/java/Guia3_Bucles_Break_Continue/Ejercicio10.java
+++ src/main/java/Guia3_Bucles_Break_Continue/Ejercicio10.java
@@ -1,8 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/*Escriba un programa en el cual se ingrese un valor límite positivo, y a continuación solicite
+números al usuario hasta que la suma de los números introducidos supere el límite inicial.
  */
 package Guia3_Bucles_Break_Continue;
+
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +15,18 @@ public class Ejercicio10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Scanner leer = new Scanner(System.in);
+       int limite = 100;
+       System.out.println("el limite es " + limite);
+        int suma = 0;
+       do {
+           System.out.println("ingrese un numero");
+           int num = leer.nextInt();
+          
+           suma = suma + num;
+       }
+         while (suma < limite);  
+       
     }
     
 }
