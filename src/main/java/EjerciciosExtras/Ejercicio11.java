f295825b848e6b64d3e4b77f27d02a336cfd4de1
silff <128050929+silff@users.noreply.github.com>
2023-03-27 20:51:50 -0300

    Update Ejercicio11.java

--- src/main/java/EjerciciosExtras/Ejercicio11.java
+++ src/main/java/EjerciciosExtras/Ejercicio11.java
@@ -13,13 +13,24 @@ public class Ejercicio11 {
     public static void main(String[] args) {
 
         Scanner leer = new Scanner(System.in);
-        
-        int num;
+
+        int num, count = 0, aux;
         do {
             System.out.println("ingrese un numero entero");
+
+            while (!leer.hasNextInt()) {
+                System.out.println("no es un entero");
+                leer.next();
+            }
             num = leer.nextInt();
+
+        } while (num < 0);
+
+        do {
+            aux = num / 10;
+            num = aux;
+            count++;
         } while (num > 0);
-
+        System.out.println("el numero ingresado tiene " + count + " digitos");
     }
-
 }
