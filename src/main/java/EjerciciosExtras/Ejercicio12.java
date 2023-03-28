d924af56074b42132c9d7e7ab9902944188c8201
silff <128050929+silff@users.noreply.github.com>
2023-03-28 08:54:18 -0300

    Create Ejercicio12.java

---
+++ src/main/java/EjerciciosExtras/Ejercicio12.java
@@ -0,0 +1,32 @@
+/*
+ Necesitamos mostrar un contador con 3 dígitos (X-X-X), que muestre los números del 0-0-
+0 al 9-9-9, con la particularidad que cada vez que aparezca un 3 lo sustituya por una E.
+Ejemplo:
+0-0-0
+0-0-1
+0-0-2
+0-0-E
+0-0-4
+0-1-2
+0-1-E
+Nota: investigar función equals() y como convertir números a String.
+ */
+package EjerciciosExtras;
+
+public class Ejercicio12 {
+
+    public static void main(String[] args) {
+        String contador = "";
+        for (int i = 0; i < 1000; i++) {
+            contador = String.valueOf(i);
+             System.out.println(contador);
+        }
+       
+    }
+    
+   /*public static void completar() {
+        
+        
+    }*/
+    
+}
