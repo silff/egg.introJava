a3555271a0035f136b3858d5ac747e02aa1b0137
silff <128050929+silff@users.noreply.github.com>
2023-04-12 19:16:13 -0300

    Update Ejercicio23.java

--- src/main/java/EjerciciosExtras/Ejercicio23.java
+++ src/main/java/EjerciciosExtras/Ejercicio23.java
@@ -10,10 +10,80 @@ funciones de Java substring(), Length() y Math.random().
  */
 package EjerciciosExtras;
 
+import java.util.Scanner;
+
 public class Ejercicio23 {
 
+    static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
+        int palabras = 5;
+        int n = 7;
+        String[][] sopa = new String[n][n];
+        llenarSopa(sopa, n);
+        for (int i = 0; i < palabras; i++) {
+            String palabra = llenarPalabra();
+            sopa(palabra, sopa, n);
+        }
+        mostrarSopa(sopa, n);
     }
-    
+
+    public static String llenarPalabra() {
+        String p;
+        do {
+            System.out.println("ingrese una palabra de entre 3 y 5 caracteres");
+            p = leer.nextLine();
+            if (p.length() < 3) {
+                System.out.println("tiene menos de 3 caracteres");
+            } else if (p.length() > 5) {
+                System.out.println("tiene mas de 5 caractes");
+            }
+        } while (p.length() < 3 || p.length() > 5);
+        return p;
+    }
+
+    public static void llenarSopa(String[][] s, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s[i][j] = " ";//String.valueOf((int) (Math.random() * 9 + 1));
+            }
+        }
+    }
+
+    @SuppressWarnings("empty-statement")
+    public static void sopa(String p, String[][] s, int n) {
+        boolean repetido;
+        do {
+
+            int x = 0;
+            int k = (int) (Math.random() * n);
+            int l = (int) (Math.abs(Math.random() * n - p.length()));
+
+            System.out.println(k + "|" + l);
+            System.out.println(p);
+
+            for (int j = l; j < l + p.length(); j++) {
+                if (s[k][j].equals(" ")) {
+                    for (int m = l; m < l + p.length(); m++) {
+                        s[k][m] = p.substring(x, x + 1);
+                        x++;
+                    }
+                } else {
+                    repetido = true;
+                    break;
+                }
+            }
+        } while (repetido = false);
+    }
+
+    public static void mostrarSopa(String[][] s, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("---------------------------------------------------------------------------------");
+            for (int j = 0; j < n; j++) {
+                System.out.print(s[i][j] + " | ");
+            }
+            System.out.println("");
+        }
+    }
 }
